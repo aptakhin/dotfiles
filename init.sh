ea2299ae051de70825223437dba688cdba49ad2f
Alexander Ptakhin <mail@aptakhin.name>
2020-08-18 18:56:57 +0300

    Initial

---
+++ init.sh
@@ -0,0 +1,29 @@
+# Install brew
+
+brew install jupyterlab
+brew install pipenv
+brew install dive
+brew install telnet
+brew install postgres
+brew install ansible ansible-playbook
+brew install ansible-decrypt
+brew install pyenv
+brew install pyenv-virtualenv
+brew install coreutils
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install 1password 
+brew cask install slack 
+brew cask install visual-studio-code
+brew cask install sourcetree
+brew cask install discord
+brew cask install tunnelblick
+brew cask install docker 
+brew cask install tunnelbear
+brew cask install jetbrains-toolbox
+brew cask install github
+brew cask install zoomus
+
+curl -O 'https://builds.clickhouse.tech/master/macos/clickhouse' && chmod a+x ./clickhouse
+mv clickhouse /usr/local/bin
