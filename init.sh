dbf6aab0156e3a15f70e221e55679fe7fe976aa2
Alexander Ptakhin <me@aptakhin.name>
2021-12-20 22:26:32 +0300

    Update sh

--- init.sh
+++ init.sh
@@ -1,28 +1,60 @@
 # Install brew
 
-brew install jupyterlab
-brew install pipenv
-brew install dive
-brew install telnet
-brew install postgres
+brew install ansible
 brew install ansible ansible-playbook
 brew install ansible-decrypt
+brew install autossh
+brew install avro-c
+brew install awscli
+brew install ca-certificates
+brew install cask github
+brew install cask google-chrome
+brew install cask iterm2
+brew install cask minikube
+brew install cask skype
+brew install cask visual-studio-code
+brew install cmake
+brew install coreutils
+brew install curl
+brew install curl-openssl
+brew install dive
+brew install gcc
+brew install gettext
+brew install git
+brew install git-lfs
+brew install go
+brew install grpc
+brew install hugo
+brew install jq
+brew install jupyterlab
+brew install kcat
+brew install ncurses
+brew install pandoc
+brew install pipenv
+brew install poetry
+brew install postgres
 brew install pyenv
 brew install pyenv-virtualenv
-brew install coreutils
+brew install python@3.10
+brew install sqlite
+brew install telnet
+brew install tmux
+brew install wget
+brew install zsh
+brew install zsh-completions
 
-brew cask install iterm2
-brew cask install google-chrome
-brew cask install 1password 
-brew cask install slack 
-brew cask install visual-studio-code
-brew cask install sourcetree
+brew cask install 1password
 brew cask install discord
-brew cask install tunnelblick
-brew cask install docker 
-brew cask install tunnelbear
-brew cask install jetbrains-toolbox
+brew cask install docker
 brew cask install github
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install jetbrains-toolbox
+brew cask install slack
+brew cask install sourcetree
+brew cask install tunnelbear
+brew cask install tunnelblick
+brew cask install visual-studio-code
 brew cask install zoomus
 
 curl -O 'https://builds.clickhouse.tech/master/macos/clickhouse' && chmod a+x ./clickhouse
